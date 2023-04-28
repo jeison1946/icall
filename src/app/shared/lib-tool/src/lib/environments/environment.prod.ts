6c9c4bd57af4b0e9284db9e8eb1aee6b27b222bb
Json Jimenez <jeison@jeison.com>
2023-04-27 19:33:13 -0500

    update fix

--- src/app/shared/lib-tool/src/lib/environments/environment.prod.ts
+++ src/app/shared/lib-tool/src/lib/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://valuable-luck-act.glitch.me/',
-  apiWebSockets: 'https://valuable-luck-act.glitch.me'
+  apiWebSockets: 'https://jumbled-clover-pot.glitch.me/'
 };
